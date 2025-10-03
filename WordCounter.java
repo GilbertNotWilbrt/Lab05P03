5310f12bc56e47b3ac4a00531985aab3b9d381d5
Gilbert <onlyingamebro@gmail.com>
2025-10-03 12:00:45 -0400

    Initial sharing of project

---
+++ WordCounter.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Keep a record of how many times each word was entered by users.
+ * 
+ * @author  Michael Kölling and David J. Barnes
+ * @version 7.0
+ */
+public class WordCounter
+{
+    // Associate each word with a count.
+    private final HashMap<String, Integer> counts;
+
+    /**
+     * Create a WordCounter
+     */
+    public WordCounter()
+    {
+        counts = new HashMap<>();
+    }
+    
+    public void printWordCounts()
+    {
+        for (String word : counts. keySet()) {
+            System.out.println(word + ": " + counts.get(word));
+        }
+    }
+    
+    /**
+     * Update the usage count of all words in input.
+     * @param input A set of words entered by the user.
+     */
+    public void addWords(HashSet<String> input)
+    {
+        for(String word : input) {
+            int counter = counts.getOrDefault(word, 0);
+            counts.put(word, counter + 1);
+        }
+    }
+    
+    public void printUnmappedWordCounts(HashMap<String, String> responseMap)
+    {
+        for (String word : counts.keySet()) {
+            if (!responseMap.containsKey(word)) {
+                System.out.println(word + ": " + counts.get(word));
+        }
+    }   
+}
+}
